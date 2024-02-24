c3722ee65b00b50912ba02d2c520c53daed9be95
McNellis <12001004017332@FEDIDCARD.GOV>
2024-02-23 15:18:33 -1000

    finished beetle script

--- exec/analyze_beetles.R
+++ exec/analyze_beetles.R
@@ -60,10 +60,74 @@ bl_all <- bl_all[, order(colnames(bl_all))]
 
 # Remove extraneous columns
 # Remove extraneous taxonomic information
-bl_all <- bl_all[, -which(colnames(bl_all) %in% c('class', 'family', 'genus', 'kingdom', 'order', 'phylum'))]
+bl_all <- bl_all[, -which(colnames(bl_all) %in% c(
+  'class', 'family', 'genus', 'kingdom', 'order', 'phylum', 'specificEpithet', 'subfamily', 'subgenus', 'tribe',
+  'infraspecificEpithet'
+))]
 # Remove extraneous sample identification
 # This assumes that the expert identification supersedes the parataxonomist identification
 bl_all$scientificName_coal <- dplyr::coalesce(bl_all$scientificName_expe, bl_all$scientificName_para)
 bl_all$scientificName_coal <- ifelse(is.na(bl_all$scientificName_expe), bl_all$scientificName_coal, bl_all$scientificName_expe)
-# What should supercede - the scientific name from the sorting, or the para/expe ID's?
+# 'scientificName' from `bet_sorting` is the same as from `scientificName_para`
+bl_all$scientificName <- bl_all$scientificName_coal
 bl_all <- bl_all[, -which(colnames(bl_all) %in% c('scientificName_coal', 'scientificName_para', 'scientificName_expe'))]
+# Simliar to identification, 'nativeStatusCode' is the same as 'nativeStatusCode_para'
+bl_all$nativeStatusCode <- dplyr::coalesce(bl_all$nativeStatusCode_expe, bl_all$nativeStatusCode_para)
+bl_all$nativeStatusCode <- ifelse(is.na(bl_all$nativeStatusCode_expe), bl_all$nativeStatusCode, bl_all$nativeStatusCode_expe)
+bl_all <- bl_all[, -which(colnames(bl_all) %in% c('nativeStatusCode_para', 'nativeStatusCode_expe'))]
+bl_all <- bl_all[, -which(colnames(bl_all) %in% c(
+  'identifiedBy', 'identifiedBy_expe', 'identifiedBy_para', 'identifiedDate', 'identifiedDate_para', 'identifiedDate_expe',
+  'identificationHistoryID', 'identificationHistoryID_expe', 'identificationHistoryID_para',
+  'identificationQualifier', 'identificationQualifier_expe', 'identificationQualifier_para',
+  'identificationReferences', 'identificationReferences_expe', 'identificationReferences_para'
+))]
+# Simliar to identification, 'morphospeciesID' is the same as 'morphospeciesID_para'
+bl_all$morphospeciesID <- dplyr::coalesce(bl_all$morphospeciesID_expe, bl_all$morphospeciesID_para)
+bl_all$morphospeciesID <- ifelse(is.na(bl_all$morphospeciesID_expe), bl_all$morphospeciesID, bl_all$morphospeciesID_expe)
+bl_all <- bl_all[, -which(colnames(bl_all) %in% c('morphospeciesID_para', 'morphospeciesID_expe'))]
+# Simliar to identification, 'scientificNameAuthorship' is the same as 'scientificNameAuthorship_para'
+bl_all$scientificNameAuthorship <- dplyr::coalesce(bl_all$scientificNameAuthorship_expe, bl_all$scientificNameAuthorship_para)
+bl_all$scientificNameAuthorship <- ifelse(is.na(bl_all$scientificNameAuthorship_expe), bl_all$scientificNameAuthorship, bl_all$scientificNameAuthorship_expe)
+bl_all <- bl_all[, -which(colnames(bl_all) %in% c('scientificNameAuthorship_para', 'scientificNameAuthorship_expe'))]
+# Simliar to identification, 'taxonID' is the same as 'taxonID_para'
+bl_all$taxonID <- dplyr::coalesce(bl_all$taxonID_expe, bl_all$taxonID_para)
+bl_all$taxonID <- ifelse(is.na(bl_all$taxonID_expe), bl_all$taxonID, bl_all$taxonID_expe)
+bl_all <- bl_all[, -which(colnames(bl_all) %in% c('taxonID_para', 'taxonID_expe'))]
+# Simliar to identification, 'taxonRank' is the same as 'taxonRank_para'
+bl_all$taxonRank <- dplyr::coalesce(bl_all$taxonRank_expe, bl_all$taxonRank_para)
+bl_all$taxonRank <- ifelse(is.na(bl_all$taxonRank_expe), bl_all$taxonRank, bl_all$taxonRank_expe)
+bl_all <- bl_all[, -which(colnames(bl_all) %in% c('taxonRank_para', 'taxonRank_expe'))]
+
+# trapID taken from sort table, id table sometimes missing
+bl_all <- bl_all[, -which(colnames(bl_all) %in% c('trapID_id', 'trapID_sort'))]
+
+# sampleCondition was coalesced because all but 3 entries had only 1 non-NA entry
+bl_all$sampleCondition_para <- ifelse(bl_all$sampleCondition_para == 'OK', NA, bl_all$sampleCondition_para)
+bl_all$sampleCondition_field <- ifelse(bl_all$sampleCondition_field == 'OK', NA, bl_all$sampleCondition_field)
+bl_all$sampleCondition_sortID <- ifelse(bl_all$sampleCondition_sortID == 'OK', NA, bl_all$sampleCondition_sortID)
+bl_all$samplCondition_combined <- dplyr::coalesce(bl_all$sampleCondition_field, bl_all$sampleCondition_sortID, bl_all$sampleCondition_para)
+bl_all <- bl_all[, -which(colnames(bl_all) %in% c('sampleCondition_field', 'sampleCondition_sortID', 'sampleCondition_para', 'sampleCondition_expe'))]
+
+# Remove other extraneous columns
+bl_all <- bl_all[, -which(colnames(bl_all) %in% c(
+  'uid_expe', 'uid_field', 'uid_para', 'uid_sortID', 'decimalLatitude', 'decimalLongitude', 'geodeticDatum', 'laboratoryName',
+  'coordinateUncertainty', 'elevationUncertainty', 'nlcdClass', 'etOHChangeDate', 'processingDate_id', 'processingDate_sort',
+  'publicationDate_expe', 'publicationDate_field', 'publicationDate_para', 'publicationDate_sortID',
+  'recordedBy', 'recordedBy_id', 'recordedBy_sort', 'release_expe', 'release_field', 'release_para', 'release_sortID',
+  'sampleCode_field', 'sampleCode_sortID', 'totalLength', 'samplingProtocolVersion', 'subsampleCode_id', 'subsampleCode_sort',
+  'setDate', 'siteID', 'plotType', 'namedLocation', 'eventID', 'domainID', 'trappingDays', 'cupStatus', 'fluidLevel', 'lidStatus',
+  'sampleCollected', 'remarks_para', 'remarks_sortID', 'remarks_expe', 'remarks_field', 'samplingImpractical', 'identificationRemarks',
+  'samplCondition_combined'
+))]
+
+bl_all <- bl_all[which(bl_all$sampleType == 'carabid'), ]
+bl_all <- bl_all[, -which(colnames(bl_all) %in% c('sampleType'))]
+
+# Reorder columns for csv
+bl_reo <- bl_all[, c(
+  'plotID', 'elevation', 'trapID', 'collectDate', 'sampleID', 'subsampleID',
+  'individualID', 'individualCount', 'taxonID', 'scientificName', 'sex',
+  'scientificNameAuthorship', 'taxonRank', 'morphospeciesID', 'nativeStatusCode'
+)]
+
+write.csv(bl_reo, '../results/tables/bet_counts_simple.csv', row.names = F)
